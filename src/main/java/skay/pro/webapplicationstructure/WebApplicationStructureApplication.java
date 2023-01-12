03a7c7e472668f39b162036646ac2191d6cc7686
aleksandrlobanov <kitalex777@mail.ru>
2023-01-13 01:34:01 +0300

    initial commit

---
+++ src/main/java/skay/pro/webapplicationstructure/WebApplicationStructureApplication.java
@@ -0,0 +1,13 @@
+package skay.pro.webapplicationstructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skay/pro/webapplicationstructure/WebApplicationStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package skay.pro.webapplicationstructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
